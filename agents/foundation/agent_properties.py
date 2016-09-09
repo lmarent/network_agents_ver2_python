ef2477b1e732f07a07196214c797ca3e9d8c29da
root <root@transit-providers.econsim.ch-geni-net.instageni.northwestern.edu>
2016-09-09 17:44:55 -0500

    adding conf options

--- agents/foundation/agent_properties.py
+++ agents/foundation/agent_properties.py
@@ -12,12 +12,19 @@ l_port_presenter = 12000
 l_port_provider = 13000
 l_port_consumer = 14000
 
+# Address for the database server information
+addr_database = '10.10.6.1'
+port_database = 3306
+user_database = 'admin'
+user_password = 'password'
+database_name = 'Network_Simulation'
+
 # Addresses for marketplace, clockserver, and own agent address
 # are defined below.
-addr_mktplace_isp = '192.168.2.12'
-addr_mktplace_backhaul = '192.168.2.13'
-addr_clock_server = '192.168.2.12'
-addr_agent = '192.168.2.13'
+addr_mktplace_isp = '10.10.6.2'
+addr_mktplace_backhaul = '10.10.4.2'
+addr_clock_server = '10.10.6.1'
+addr_agent = '10.10.4.1'
 
 threshold = 2
 own_neighbor_radius = 0.05
@@ -25,6 +32,7 @@ others_neighbor_radius = 100 # almost every bid is in the neighbor.
 initial_number_bids = 5
 num_periods_market_share = 3
 intervals_per_cycle = 2
+provider_types = 'Provider'
 
 #directory results
 result_directory = 'results/'
